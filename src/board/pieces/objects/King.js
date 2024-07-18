0ae2e87079d3a2e760a87771f15c7558886d5e6c
w5b <w5b>
2024-07-19 02:42:54 +0300

    knight movement

--- src/board/pieces/objects/King.js
+++ src/board/pieces/objects/King.js
@@ -12,7 +12,37 @@ class King extends Piece {
   }
 
   getWalkableTiles(board) {
-    return [];
+    const walkableTiles = [];
+    const directionsSide = [this.direction.left, this.direction.right];
+    const directionsForward = [
+      this.direction.forwards,
+      this.direction.backwards,
+    ];
+    const directionsDiagonal = [
+      this.direction.leftBackWards,
+      this.direction.rightBackwards,
+      this.direction.leftForwards,
+      this.direction.rightForwards,
+    ];
+
+    for (let dir of directionsSide) {
+      const tile = this.sideTile(1, dir);
+      const walkableTilesCheck = this.simpleWalkCheck(tile, board);
+      walkableTiles.push(...walkableTilesCheck);
+    }
+
+    for (let dir of directionsForward) {
+      const tile = this.forwardTile(1, dir);
+      const walkableTilesCheck = this.simpleWalkCheck(tile, board);
+      walkableTiles.push(...walkableTilesCheck);
+    }
+
+    for (let dir of directionsDiagonal) {
+      const tile = this.diagonalTile(1, dir);
+      const walkableTilesCheck = this.simpleWalkCheck(tile, board);
+      walkableTiles.push(...walkableTilesCheck);
+    }
+    return walkableTiles;
   }
 }
 
